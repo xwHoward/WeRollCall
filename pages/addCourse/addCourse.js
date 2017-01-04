29d807fb8b16c691ad1e7b2c28e510f672923ed8
Howard <xwhoward@163.com>
2017-01-04 18:06:08 +0800

    [added] 学生选课功能

--- pages/addCourse/addCourse.js
+++ pages/addCourse/addCourse.js
@@ -1,43 +1,120 @@
 const AV = require('../../lib/leancloud-storage');
+var COURSE = AV.Object.extend('COURSE');
 var app = getApp();    
-// const user = AV.User.current();
 Page({
   data: {
-    timeStart: '08:00',
-    timeEnd: '09:35',
-    weekdays: [
-      {name: 'MON', value: '周一'},
-      {name: 'TUE', value: '周二'},
-      {name: 'THI', value: '周三'},
-      {name: 'WED', value: '周四'},
-      {name: 'FRI', value: '周五'},
-      {name: 'SUN', value: '周六'},
-    ]
+    template: 'search-course',
+    tplData: {
+      timeStart: '08:00',
+      timeEnd: '09:35',
+      weekdays: [
+        {name: '周一', value: '周一'},
+        {name: '周二', value: '周二'},
+        {name: '周三', value: '周三'},
+        {name: '周四', value: '周四'},
+        {name: '周五', value: '周五'},
+        {name: '周六', value: '周六'},
+      ]
+    }
   },
   bindTimeStartChange: function(e) {
     this.setData({
-      timeStart: e.detail.value
-    })
+      'tplData.timeStart': e.detail.value
+    });
   },
   bindTimeEndChange: function(e) {
     this.setData({
-      timeEnd: e.detail.value
-    })
+      'tplData.timeEnd': e.detail.value
+    });
   },
   formSubmit: function(e) {
     var data = e.detail.value;
-    console.log('form发生了submit事件，携带数据为：', data)
-    var COURSE = AV.Object.extend('COURSE');
     var course = new COURSE();
     course.set('courseName',data.courseName);
     course.set('courseAddr',data.courseAddr);
     course.set('courseTimeEnd',data.courseTimeEnd);
     course.set('courseTimeStart',data.courseTimeStart);
-    console.log(app.globalData.user)
-    course.set('teacher',app.globalData.user);
+    var teacher = AV.Object.createWithoutData('_User', app.globalData.user.objectId);
+    course.set('teacher',teacher);
     course.set('weekdays',data.weekdays);
+    course.save().then(function (course) {
+      console.log("add course success!");
+      //添加课程成功，继续添加或返回
+      wx.showModal({
+        title: '添加课程成功',
+        icon: 'success',
+        content: '您可以继续添加课程或者返回主页',
+        cancelText: '继续添加',
+        cancelColor: '#1AAD16',
+        confirmText: '返回主页',
+        confirmColor: '#3CC51F',
+        success: function(res) {
+          if (res.confirm) {//返回主页
+            console.log('返回主页')
+            wx.navigateBack();
+          } else if (res.cancel){
+            console.log('继续添加')
+            wx.hideToast();
+          }
+        }
+      });
+    }, function (error) {
+      console.error(error);
+    });
+  },
+  chooseCourse: function(e){
+    var student = AV.Object.createWithoutData('_User', app.globalData.user.objectId);
+    var course = AV.Object.createWithoutData('COURSE', e.target.dataset.course.objectId);
+    student.addUnique('coursesChosen', course);
+    student.save().then(function(stu){
+      console.log('choose course success!')
+      wx.showModal({
+        title: '添加课程成功',
+        icon: 'success',
+        content: '您可以继续添加课程或者返回主页',
+        cancelText: '继续添加',
+        cancelColor: '#1AAD16',
+        confirmText: '返回主页',
+        confirmColor: '#3CC51F',
+        success: function(res) {
+          if (res.confirm) {//返回主页
+            console.log('返回主页')
+            wx.navigateBack();
+          } else if (res.cancel){
+            console.log('继续添加')
+            wx.hideToast();
+          }
+        }
+      });
+    }, function (error) {
+      console.error(error);
+    });
+  },
+  searchCourse: function(e) {
+    var that = this;
+    var query = new AV.Query('COURSE');
+    query.contains('courseName', e.detail.value);
+    query.include('teacher');
+    query.include('teacher.userName');
+    query.find().then(function (results) {
+      for (var i = 0; i < results.length; i++) {
+          var result = results[i];
+          // 并不需要网络访问
+          // var courseName = result.get('courseName');
+          var teacher = result.get('teacher');
+          var teacherName = teacher.get('userName');
+          results[i].attributes.teacherName = teacherName;
+      }
+      that.setData({
+        'tplData.results': results
+      });
+    }, function (error) {
+      console.log(error)
+    });
   },
   onLoad: function () {
-
+    this.setData({
+      'tplData.courseTeacher': app.globalData.user.userName
+    })
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -3,8 +3,14 @@ const AV = require('../../lib/leancloud-storage');
 var app = getApp();
 Page({
   data: {
-    userInfo: {}
+    userInfo: {},
+    courses:[]
   },
+  onPullDownRefresh: function(){
+    this.initData();
+    wx.stopPullDownRefresh();
+  },
+  //添加新课程
   addCourse: function(){
     wx.navigateTo({
       url: '../addCourse/addCourse',
@@ -19,13 +25,46 @@ Page({
       }
     })
   },
-  onLoad: function () {
+  initData: function(){
     var that = this;
-    console.log("Enter index page")
+    if(app.globalData.user.userType === "老师"){
+      //以教师身份登录，初始化教师所授课程
+      var queryCourse = new AV.Query('COURSE');
+      console.log(app.globalData.user)
+      var teacher = AV.Object.createWithoutData('_User', app.globalData.user.objectId);
+      queryCourse.equalTo('teacher', teacher);
+      queryCourse.find().then(function (results) {
+        console.log(results)
+        that.setData({
+          courses: results
+        });
+      }, function (error) {
+      });
+    } else {
+      //以学生身份登录
+      //初始化学生所选课程
+      console.log("usertype:student")
+      //从coursesChosen字段中取出id，到COURSE表中查找相应的行
+      var results = [];
+      app.globalData.user.coursesChosen.forEach(function(e){
+        var queryCourses = new AV.Query('COURSE');
+        queryCourses.equalTo('objectId',e.objectId);
+        queryCourses.find().then(function(course){
+          results.push(course)
+          that.setData({
+            courses: results
+          });
+        })
+      });
+    }
+  },
+  //初始化用户信息
+  //返回用户类型：'教师'/'学生'
+  initUserInfo: function(cb){
+    var that = this;
     // 获得当前登录用户
     const user = AV.User.current();
-    console.log("AV.User.current() already in app when page.index onload:")
-    console.log(user)
+    console.log("AV.User.current() already in app when page.index onload:",user)
     console.log("app.globalData.user already in app when page.index onload:",app.globalData.user)
     if(user == null){
         // 首次登陆需要初始化身份数据
@@ -47,18 +86,31 @@ Page({
       // 调用小程序 API，得到用户信息
       wx.getUserInfo({
         success: ({userInfo}) => {
-          console.log('wx.getUserInfo success, returned wx userInfo:')
-          console.log(userInfo)
+          console.log('wx.getUserInfo success, returned wx userInfo:',userInfo)
           // 更新当前用户的信息
           user.set(userInfo).save().then(user => {
             // 成功，此时可在控制台中看到更新后的用户信息
             app.globalData.user = user.toJSON();
-            console.log("Update userinfo on leanCloud success, returned new user info:")
-            console.log(app.globalData.user)
+            console.log("Update userinfo on leanCloud success, returned new user info:",app.globalData.user)
+            cb();
           }).catch(console.error);
+        },
+        fail: function(){
+          console.log("Update userinfo on leanCloud FAILED!")
+          app.globalData.user = user.toJSON();
+          cb();
+        },
+        complete: function(){
         }
-      }); 
+      });
     }
+  },
+  onLoad: function () {
+    console.log("Enter index page")
+    this.initUserInfo(this.initData);
+  },
+  onShow: function(){
+    
   }
   // ,isEmptyObj: function(obj){
   //   for (var a in obj){
